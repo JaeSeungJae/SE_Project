7fe4f79c213ba096958976b8e758d628381aafdf
HaechangLee <112938092+HaechangLee@users.noreply.github.com>
2024-06-07 14:13:43 +0900

    db change impl

--- src/main/java/com/hc/demo/container/User.java
+++ src/main/java/com/hc/demo/container/User.java
@@ -1,15 +1,16 @@
 package com.hc.demo.container;
 
 public class User {
-    private int uid, reservedKRW;
+    private int uid, reservedKRW, level;
     private String ID, PW, Name, Nickname;
 
-    public User(int uid, String ID, String PW, String Name, String Nickname, int reservedKRW) {
+    public User(int uid, String ID, String PW, String Name, String Nickname, int level, int reservedKRW) {
         this.uid = uid;
         this.ID = ID;
         this.PW = PW;
         this.Name = Name;
         this.Nickname = Nickname;
+        this.level = level;
         this.reservedKRW = reservedKRW;
     }
 
@@ -23,6 +24,10 @@ public class User {
 
     public String getNickname() {
         return Nickname;
+    }
+
+    public int getLevel() {
+        return level;
     }
 
     public void setNickname(String nickname) {
